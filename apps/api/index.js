c8a017249d65a11800266336558e9d187895c3e2
aslampangestu <aslampangestu@gmail.com>
2022-01-24 12:06:27 +0700

    add mock data

--- apps/api/index.js
+++ apps/api/index.js
@@ -1,15 +1,25 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 8080;
+const { blogData, profileData } = require("./data");
+
+const PORT = process.env.PORT || 8080;
 
 app.get("/api/v1/blogs", (req, res) => {
-  res.send("Ini Blog");
+  res.send({
+    status: true,
+    message: "Berhasil mendapat data blog",
+    data: blogData,
+  });
 });
 
 app.get("/api/v1/profile", (req, res) => {
-  res.send("Ini Profile");
+  res.send({
+    status: true,
+    message: "Berhasil mendapat data profile",
+    data: profileData,
+  });
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
 });
