91dadf08119547edf0760160b1f6e717fdedaa6c
aslampangestu <aslampangestu@gmail.com>
2022-01-24 11:46:12 +0700

    update config

--- apps/api/index.js
+++ apps/api/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
-const port = 3002;
+const port = process.env.PORT || 8080;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/api/v1/blogs", (req, res) => {
+  res.send("Ini Blog");
+});
+
+app.get("/api/v1/profile", (req, res) => {
+  res.send("Ini Profile");
 });
 
 app.listen(port, () => {
